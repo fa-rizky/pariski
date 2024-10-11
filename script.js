145e79efef9a562721c447e0ce295af0d73b73b8
fa-rizky <farizky.ghaisan@gmail.com>
2024-10-11 09:54:11 +0700

    Commit pertama

---
+++ script.js
@@ -0,0 +1,11 @@
+// var a = 10;
+// var b = 23;
+
+// alert(a+b)
+
+
+var namaInput = prompt ('Masukan Nama Kamu');
+var usia = prompt ('Silahkan masukan umur anda')
+alert(`selamat datang ${namaInput} dan berumur ${usia}di website kami`);
+
+
